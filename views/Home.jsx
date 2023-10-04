1b443ba788348ca18dbebc1c5ca7e84055258647
FedeOrefici <federicoaorefici@gmail.com>
2023-10-04 20:10:57 +0200

    update

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,9 +1,20 @@
+import { useContext } from "react"
+import { ContextData } from "../context/ContextData"
 
 
 const Home = () => {
+
+    const {products} = useContext(ContextData)
+    
   return (
     <div>
-      home
+      {products?.drinks?.map((product) => (
+        <div key={product.idDrink}>
+            <p>{product.strGlass}</p>
+            <img src={product.strDrinkThumb} />
+            <p>{product.strInstructions}</p>
+        </div>
+      ))}
     </div>
   )
 }
