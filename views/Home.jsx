31c1fc085a82a9035c51a3d638925762d3c8f93a
FedeOrefici <federicoaorefici@gmail.com>
2023-10-13 13:21:26 +0200

    update

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,14 +1,14 @@
-import { useContext } from "react"
-import { ContextData } from "../context/ContextData"
 import Card from "../src/components/Card"
+import SearchBar from "../src/components/SearchBar"
 
 
 const Home = () => {
 
-    const {products} = useContext(ContextData)
+    
     
   return (
-    <div className="w-full mt-10 flex items-center justify-center bg-blue-700 h-screen">
+    <div>
+      <SearchBar />
       <Card />
     </div>
   )
