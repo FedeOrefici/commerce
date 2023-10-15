3f1fe2a4db9486466ba56ebb70241c1aaaf1b9e1
FedeOrefici <federicoaorefici@gmail.com>
2023-10-15 13:50:11 +0200

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,12 @@ const Card = () => {
 
     const {products, addFavs, favs, filteredData} = useContext(ContextData)
 
-  const displayData = filteredData.length > 0 ? filteredData : products
+    const displayData = filteredData.length > 0 ? filteredData : products || []
 
 
   return (
     <div className='grid items-center justify-center grid-cols-3 gap-4 p-4'>
-     {displayData.map((product) => (
+     {displayData?.drinks?.map((product) => (
         <div key={product.idDrink} className="bg-slate-300 w-[300px] h-[400px] border rounded p-4">
             <div onClick={()=> addFavs(product.idDrink)} className='cursor-pointer'>
               {product.idDrink === favs ? <span>favorito</span>
