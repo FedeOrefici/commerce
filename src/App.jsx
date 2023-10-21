b3a5577b8b2ac0a3e1496aff26f0b96c65dd9a8b
FedeOrefici <federicoaorefici@gmail.com>
2023-10-21 18:56:20 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../views/Home"
-import Navbar from "./components/Navbar"
 import Favorites from "../views/Favorites"
 import Login from "../views/Login"
+import ItemDetail from "./components/ItemDetail"
 
 function App() {
-
-
   return (
     <div>
-
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="home" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
+        <Route path="/:id" element={<ItemDetail />} />
       </Routes>
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,29 @@
 import React, { useContext, useState } from 'react'
 import { ContextData } from '../../context/ContextData'
 import Loader from './Loader'
+import { NavLink } from 'react-router-dom'
 
 const Card = () => {
 
     const {addFavs, isSearching, currentItems} = useContext(ContextData)
 
-
+    
 
   return (
-    <div className='grid items-center justify-center grid-cols-3 gap-4 p-4'>
+    <div className='flex items-center justify-center p-4 min-h-[600px]'>
      {isSearching ? (<Loader />) : (
       currentItems?.map((product) => (
-        <div key={product.id} className="bg-slate-300 w-[300px] h-[400px] border rounded p-4">
+        <div key={product.id} className="bg-slate-300 w-[300px] h-[200px] border rounded p-4">
             <div onClick={()=> addFavs(product.id)} className='cursor-pointer'>
               {product.isFavorite ? <span>favorito</span>
               : <span>no favorito</span>} 
             </div>
             <p>{product.name}</p>
             <img style={{width:'80px'}} src={product.image} />
-            <p className='p-2 w-full text-[12px]'>{product.description}</p>
+            <p className='p-2 w-full text-[12px]'>{product.description.slice(0, 40)}...</p>
+            <NavLink to={`/${product.id}`}>
+              <button className='bg-violet-800 text-white rounded p-2 w-[150px]'>more</button>
+            </NavLink>
         </div>
       ))) }
       
