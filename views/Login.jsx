1739badc7d1a5881c827dcffd86dc793067cb0bd
FedeOrefici <federicoaorefici@gmail.com>
2023-10-24 13:41:38 +0200

    update

--- views/Login.jsx
+++ views/Login.jsx
@@ -11,11 +11,11 @@ const Login = () => {
 
 
   return (
-    <div>
-      <form>
-        <input type='text' placeholder='user' />
-        <input type='password' placeholder='password' />
-        <button onClick={handleLogin}>login</button>
+    <div className='w-full h-[600px] border border-gray-950 flex items-center justify-center bg-[#7A293D]'>
+      <form className='flex items-center justify-center flex-col mt-[100px] bg-red-400'>
+        <input className='bg-red-500' type='text' placeholder='user' />
+        <input className='bg-red-500' type='password' placeholder='password' />
+        <button onClick={handleLogin} className='bg-slate-500 w-[100px] h-[30px]'>login</button>
       </form>
     </div>
   )
